988467b70d77d2c6e7b900dd5be29d19787e0d54
Vitaliy Mkhchyan <vitaliy.mkhchyan@gmail.com>
2023-07-10 16:38:15 +0300

    Update install.sh

--- bash_scripts/install.sh
+++ bash_scripts/install.sh
@@ -2,12 +2,12 @@
 echo "[+] Start of installation..."
 mv __main__.py style.py
 chmod +x style.py
-mkdir -p /home/$USER/.apps/Styles
-mv style.py /home/$USER/.apps/Styles
+mkdir -p /home/"$USER"/.apps/Styles
+mv style.py /home/"$USER"/.apps/Styles
 echo "[Desktop Entry]
 Encoding=UTF-8
 Type=Application
 Terminal=false
 Exec=/usr/bin/python3 /home/$USER/.apps/Styles/style.py
-" > /home/$USER/.config/autostart/style.desktop
+" > /home/"$USER"/.config/autostart/style.desktop
 echo "[+] Installation is complete!"
